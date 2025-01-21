a7c653849196c2bfa5982aeea566e6b77ad22a2f
Eirado <111468283+Eirado@users.noreply.github.com>
2025-01-21 16:59:00 -0300

    floor in BST

--- DataStructure/BinarySearchTree.py
+++ DataStructure/BinarySearchTree.py
@@ -51,17 +51,45 @@ class BST:
         return node
 
 
+    def floor(self, k):
+
+        x = self._floor(self.root, k)
+        if x is None:
+            return None
+
+        print(x.key, x.value)
+        return x.key
+
+    def _floor(self, node, key):
+
+        if node is None:
+            return None
+
+        if key == node.key:
+            return node
+
+        if node.key > key: # have to keep going to the left until you find a node that is less than the original k
+            return self._floor(node.left, key)
+
+        t = self._floor(node.right, key)
+        if t is not None:
+            return t
+        else:
+            return node
+
+
 def main():
     tree = BST() # the efficiency will depend on the order that the keys comes in, this is not taking balancing in consideration
-    tree.put('F', 7)
-    tree.put('B', 2)
-    tree.put('A', 1)
-    tree.put('D', 5)
+    tree.put('S', 7)
+    tree.put('X', 2)
+    tree.put('E', 1)
+    tree.put('A', 5)
     tree.put('C', 4)
-    tree.put('E', 6)
-    tree.put('G', 8)
-    print(tree.get('G'))
-
+    tree.put('R', 6)
+    tree.put('H', 8)
+    tree.put('M', 10)
+    print(tree.get('M'))
+    tree.floor('N')
 
 if __name__ == '__main__':
     main()
---
+++ DataStructure/OrderedBST.py
@@ -0,0 +1,17 @@
+
+
+class Node:
+
+    left = None
+    right = None
+    key: any
+    value: any
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+class OrderedBST:
+
+    pass
