1352d64444f00f4eb51cf2ff3b633361e32d16bb
Eirado <111468283+Eirado@users.noreply.github.com>
2025-01-23 14:39:02 -0300

    delete fuction

--- DataStructure/BinarySearchTree.py
+++ DataStructure/BinarySearchTree.py
@@ -21,21 +21,22 @@ class BST:
 
     def get(self, key):
 
+        if self.root is None:
+            return None
 
         return self._get(self.root, key)
 
     def _get(self, node: Node, key):
 
-        while node is not None:
+            if node is None:
+                return None
+
             if node.key > key:
-                node = self._get(node.left, key)
+                return self._get(node.left, key)
             elif node.key < key:
-                node = self._get(node.right, key)
+                return self._get(node.right, key)
             else:
-                return node
-
-        return None
-
+                return node.value
 
 
     def put(self, key, value):
@@ -66,28 +67,46 @@ class BST:
         return x.key
 
 
-    # def delete(self, key):
-    #     return self._delete(self.root, key)
-    #
-    # def _delete(self, node, key):
-    #
-    #
-    #
-    #     while node is not None:
-    #
-    #         if node.key > key:
-    #             node = node.left
-    #         elif node.key < key:
-    #             node = node.right
-    #         else:
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #      return None
+    def delete(self, key):
+
+        if self.root is None:
+            return None
+
+        return self._delete(self.root, key)
+
+    def _delete(self, node, key):
+
+        if node is None:
+            print('Does not exist')
+            return None
+
+        if node.key > key:
+            node.left = self._delete(node.left, key)
+        elif node.key < key:
+            node.right = self._delete(node.right, key)
+        else: #
+            if node.left is None and node.right is None:
+                node = None
+            elif node.left is None and node.right is not None:
+                node = node.right
+            elif node.right is None and node.left is not None:
+                node = node.left
+            else:
+
+                smallest_node: Node = node.right
+
+                while smallest_node.left is not None:
+                    smallest_node = smallest_node.left
+
+                node.key = smallest_node.key
+                node.value = smallest_node.value
+
+                smallest_node = self._delete(smallest_node, smallest_node.key)
+
+
+
+
+        return node
 
     def _floor(self, node, key):
 
@@ -109,16 +128,29 @@ class BST:
 
 def main():
     tree = BST() # the efficiency will depend on the order that the keys comes in, this is not taking balancing in consideration
-    tree.put('S', 7)
-    tree.put('X', 2)
-    tree.put('E', 1)
-    tree.put('A', 5)
-    tree.put('C', 4)
-    tree.put('R', 6)
-    tree.put('H', 8)
-    tree.put('M', 10)
-    print(tree.get('M').value)
-    tree.floor('N')
+    tree.put(7, 'S')
+    tree.put(5, 'X')
+    tree.put(20, 'E')
+    tree.put(4, 'A')
+    tree.put(18, 'C')
+    tree.put(25, 'R')
+    tree.put(2, 'H')
+    tree.put(11, 'M')
+    tree.put(19, 'M')
+    tree.put(33, 'M')
+    tree.put(1, 'M')
+    tree.put(3, 'M')
+    tree.put(14, 'M')
+    tree.put(28, 'M')
+    tree.put(12, 'M')
+    tree.put(15, 'M')
+    tree.put(31, 'M')
+
+    print(tree.get(7))
+    tree.floor(29)
+    tree.delete(7)
+    print(tree.get(7))
+    print(tree.get(14))
 
 if __name__ == '__main__':
     main()
